519137394681e10e2b4bbf925ddaf00503ae84d0
maliabhinav-tech <maliabhinav2014@gmail.com>
2026-08-08 09:20:03 +0530

    add to gitinore

---
+++ personal-website/src/App.jsx
@@ -0,0 +1,11 @@
+import ShowcasePage from "./component/header/Header"
+
+function App() {
+  return (
+    <div className="App">
+      <ShowcasePage />
+    </div>
+  )
+}
+
+export default App
---
+++ personal-website/src/component/header/Header.tsx
@@ -0,0 +1,11 @@
+export default function ShowcasePage() {
+  return (
+    <div style={{ width: '100%', height: '100vh', border: 'none' }}>
+      <iframe
+        src="/login pages design/index.html"
+        title="Login Form Project"
+        style={{ width: '100%', height: '100%', }}
+      />
+    </div>
+  );
+}
