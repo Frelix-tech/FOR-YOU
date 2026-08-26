29acacf1a1879ae874ad05551abc9dcefb4ca48f
maliabhinav-tech <maliabhinav2014@gmail.com>
2026-08-26 19:54:53 +0530

    starting to make the project

---
+++ love/src/enter/enter.jsx
@@ -0,0 +1,21 @@
+import './enter.css'
+function Love() {
+
+ return (
+  <div className="box1 hover">
+    <div className="head1">
+      <h1 >
+        Hey There ! 
+      </h1>
+      <p >
+        I have make some thing for you by own .<br/> Look completly 
+      </p>
+      <button className="button1"> 
+        Lets Move Forward ✨
+      </button>
+    </div>
+  </div>
+ )
+}
+
+export default Love
